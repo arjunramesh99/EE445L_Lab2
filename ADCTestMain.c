84ec0b6011b7c5ce04312d9d6852bf239dcd9da6
arjunramesh99 <36214038+arjunramesh99@users.noreply.github.com>
2019-09-16 16:06:14 -0500

    Files added

---
+++ ADCTestMain.c
@@ -0,0 +1,152 @@
+// ADCTestMain.c
+// Runs on TM4C123
+// This program periodically samples ADC channel 0 and stores the
+// result to a global variable that can be accessed with the JTAG
+// debugger and viewed with the variable watch feature.
+// Daniel Valvano
+// September 5, 2015
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// center of X-ohm potentiometer connected to PE3/AIN0
+// bottom of X-ohm potentiometer connected to ground
+// top of X-ohm potentiometer connected to +3.3V 
+#include <stdint.h>
+#include "ADCSWTrigger.h"
+#include "../inc/tm4c123gh6pm.h"
+#include "PLL.h"
+#include "ST7735.h"
+
+#define PF2             (*((volatile uint32_t *)0x40025010))
+#define PF1             (*((volatile uint32_t *)0x40025008))
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+
+volatile uint32_t ADCvalue;
+// This debug function initializes Timer0A to request interrupts
+// at a 100 Hz frequency.  It is similar to FreqMeasure.c.
+void Timer0A_Init100HzInt(void){
+  volatile uint32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 79999;         // start value for 100 Hz interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+
+void Timer1_Init(void) { 
+	volatile uint32_t delay;
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  delay = SYSCTL_RCGCTIMER_R;   // allow time to finish activating
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, down-count 
+  TIMER1_TAILR_R = 0xFFFFFFFF;  // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
+}
+
+
+int ADC_dump[1000];
+int time_dump[1000];
+int slot = 0;
+int jitter_dump[999];
+int min_diff, max_diff;
+int jitter;	
+int pmf_occurences[4096] = {0};
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
+  PF2 ^= 0x04;                   // profile
+  PF2 ^= 0x04;                   // profile
+  ADCvalue = ADC0_InSeq3();
+	ADC_dump[slot] = ADCvalue;
+	time_dump[slot] = TIMER1_TAR_R;
+	slot++;
+	if (slot == 1000)
+			TIMER0_CTL_R &= ~TIMER_CTL_TAEN;
+	pmf_occurences[ADCvalue]++;
+  PF2 ^= 0x04;                   // profile
+}
+
+void getMinMax(int arr[], int size, int* max_val, int* min_val) {
+	int min = arr[0], max = arr[0];
+	for (int i = 1; i < size; i++) {
+		if(arr[i] < min)
+			min = arr[i];
+		if(arr[i] > max)
+			max = arr[i];
+	}
+	*max_val = max;
+	*min_val = min;
+}
+
+
+void plotPMF() {
+	ST7735_PlotClear(0, 127);
+	for (int i = 0; i < 128; i++) {
+		ST7735_PlotBar(pmf_occurences[i]/4);
+		ST7735_PlotNext();
+	}
+}
+	
+int main(void){
+  PLL_Init(Bus80MHz);                   // 80 MHz
+  SYSCTL_RCGCGPIO_R |= 0x20;            // activate port F
+  ADC0_InitSWTriggerSeq3_Ch9();         // allow time to finish activating
+  Timer0A_Init100HzInt();               // set up Timer0A for 100 Hz interrupts
+	Timer1_Init();
+	ST7735_InitR(INITR_REDTAB);
+  GPIO_PORTF_DIR_R |= 0x06;             // make PF2, PF1 out (built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x06;          // disable alt funct on PF2, PF1
+  GPIO_PORTF_DEN_R |= 0x06;             // enable digital I/O on PF2, PF1
+                                        // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF00F)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
+  PF2 = 0;                      // turn off LED
+  EnableInterrupts();
+  while(1){
+    PF1 ^= 0x02;  // toggles when running in main
+		if (slot == 1000) {
+			for (int i = 0; i < 999; i++) {
+					jitter_dump[i] = time_dump[i] - time_dump[i+1];
+				  if (jitter_dump[i] < 0)
+						jitter_dump[i] += 0xFFFFFFFF;
+			}
+			getMinMax(jitter_dump, 999, &max_diff, &min_diff);
+			jitter = max_diff - min_diff;
+			plotPMF();
+		}
+  }
+}
+
+
---
+++ st7735_portd.d
@@ -0,0 +1,6 @@
+.\st7735_portd.o: ST7735_PortD.c
+.\st7735_portd.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\st7735_portd.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\st7735_portd.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\st7735_portd.o: ST7735_PortD.h
+.\st7735_portd.o: ../inc/tm4c123gh6pm.h
