ba4b137eb146cf48849fe33b01caa9aa3346b174
arjunramesh99 <36214038+arjunramesh99@users.noreply.github.com>
2019-09-19 14:19:26 -0500

    Completed Code

--- ADCTestMain.c
+++ ADCTestMain.c
@@ -53,7 +53,7 @@ void Timer0A_Init100HzInt(void){
   // **** timer0A initialization ****
                                    // configure for periodic mode
   TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
-  TIMER0_TAILR_R = 79999;         // start value for 100 Hz interrupts
+  TIMER0_TAILR_R = 799999;         // start value for 100 Hz interrupts
   TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
   TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
   TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
@@ -75,31 +75,58 @@ void Timer1_Init(void) {
   TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
 }
 
+void Timer2_Init(void){
+	volatile uint32_t delay;
+  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+  delay = SYSCTL_RCGCTIMER_R;          // user function
+  TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
+  TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAILR_R = 7999;    // 4) reload value
+  TIMER2_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
+  TIMER2_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0x00FFFFFF)|0x40000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 39, interrupt number 23
+  NVIC_EN0_R = 1<<23;           // 9) enable IRQ 23 in NVIC
+  TIMER2_CTL_R = 0x00000001;    // 10) enable timer2A
+}
 
 int ADC_dump[1000];
 int time_dump[1000];
 int slot = 0;
 int jitter_dump[999];
-int min_diff, max_diff;
-int jitter;	
+uint32_t min_diff, max_diff;
+uint32_t jitter;	
 int pmf_occurences[4096] = {0};
 
 void Timer0A_Handler(void){
   TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
   PF2 ^= 0x04;                   // profile
   PF2 ^= 0x04;                   // profile
-  ADCvalue = ADC0_InSeq3();
-	ADC_dump[slot] = ADCvalue;
-	time_dump[slot] = TIMER1_TAR_R;
-	slot++;
-	if (slot == 1000)
-			TIMER0_CTL_R &= ~TIMER_CTL_TAEN;
-	pmf_occurences[ADCvalue]++;
+	ADCvalue = ADC0_InSeq3();
+	if (slot < 1000) {
+		ADC_dump[slot] = ADCvalue;
+		time_dump[slot] = TIMER1_TAR_R;
+		slot++;
+	//if (slot == 1000)
+	//		TIMER0_CTL_R &= ~TIMER_CTL_TAEN;
+		pmf_occurences[ADCvalue]++;
+	}
   PF2 ^= 0x04;                   // profile
 }
 
-void getMinMax(int arr[], int size, int* max_val, int* min_val) {
-	int min = arr[0], max = arr[0];
+
+void Timer2A_Handler(void){
+  TIMER2_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER2A timeout
+	volatile int k = 2;
+	k++;
+}
+
+
+void getMinMax(int arr[], int size, uint32_t* max_val, uint32_t* min_val) {
+	uint32_t min = arr[1], max = arr[1];
 	for (int i = 1; i < size; i++) {
 		if(arr[i] < min)
 			min = arr[i];
@@ -114,7 +141,7 @@ void getMinMax(int arr[], int size, int* max_val, int* min_val) {
 void plotPMF() {
 	ST7735_PlotClear(0, 127);
 	for (int i = 0; i < 128; i++) {
-		ST7735_PlotBar(pmf_occurences[i]/4);
+		ST7735_PlotBar(pmf_occurences[i+160]);
 		ST7735_PlotNext();
 	}
 }
@@ -125,6 +152,7 @@ int main(void){
   ADC0_InitSWTriggerSeq3_Ch9();         // allow time to finish activating
   Timer0A_Init100HzInt();               // set up Timer0A for 100 Hz interrupts
 	Timer1_Init();
+	Timer2_Init();
 	ST7735_InitR(INITR_REDTAB);
   GPIO_PORTF_DIR_R |= 0x06;             // make PF2, PF1 out (built-in LED)
   GPIO_PORTF_AFSEL_R &= ~0x06;          // disable alt funct on PF2, PF1
@@ -134,9 +162,11 @@ int main(void){
   GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
   PF2 = 0;                      // turn off LED
   EnableInterrupts();
-  while(1){
+	int isDisplayed = 0;
+	while(1){
     PF1 ^= 0x02;  // toggles when running in main
-		if (slot == 1000) {
+		if (slot == 1000 && !isDisplayed) {
+			isDisplayed = 1;
 			for (int i = 0; i < 999; i++) {
 					jitter_dump[i] = time_dump[i] - time_dump[i+1];
 				  if (jitter_dump[i] < 0)
@@ -146,6 +176,7 @@ int main(void){
 			jitter = max_diff - min_diff;
 			plotPMF();
 		}
+		PF1 = (PF1*12345678)/1234567+0x02;  // this line causes jitter
   }
 }
 
